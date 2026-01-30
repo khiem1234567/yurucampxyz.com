9272429aa754bf94ba2b7f8f9a0a2cb4601c1b87
unknown <hoangkhiem7891@gmail.com>
2026-01-30 23:34:13 +0700

    upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function openInfo() {
+  document.getElementById("characterModal").style.display = "block";
+}
+function closeInfo() {
+  document.getElementById("characterModal").style.display = "none";
+}
+function openInfo1() {
+  document.getElementById("characterModal1").style.display = "block";
+}
+function closeInfo1() {
+  document.getElementById("characterModal1").style.display = "none";
+}
